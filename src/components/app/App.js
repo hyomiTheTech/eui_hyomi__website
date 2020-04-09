b74a3db3365b108447d1b907c011dd4e6575480e
David Kim <hyomithetech@gmail.com>
2020-04-09 12:35:45 -0700

    Complete basic configuration and main header

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <header className="header">
+      <div className="heading-primary">
+        <span className="heading-primary-main">David Kim</span>
+        <span className="heading-primary-sub">Interactive resume</span>
+      </div>
+    </header>
+  );
 }
 
 export default App;
