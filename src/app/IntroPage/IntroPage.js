c943edbe943e2453ab13ab6812ce5db00d786e85
David Kim <hyomithetech@gmail.com>
2020-10-16 02:51:36 -0700

    Feature: scroll-button - Create and implement css animation

--- src/app/IntroPage/IntroPage.js
+++ src/app/IntroPage/IntroPage.js
@@ -5,11 +5,15 @@ import LogoContainer from "./LogoContainer";
 function Heading() {
   return (
     <div className="wrapper">
+      <LogoContainer />
       <div className="heading__primary">
         <span className="heading__primary-main">David Kim</span>
         <span className="heading__primary-sub">Interactives resume</span>
       </div>
-      <LogoContainer />
+      <div className="scroll-button">&nbsp;START</div>
+      <a href="#" className="down-arrow">
+        <img src="./assets/images/logos/down-chevron.svg" alt="down arrow" />
+      </a>
     </div>
   );
 }
