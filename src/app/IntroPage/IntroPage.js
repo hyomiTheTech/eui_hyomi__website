0de0f15f6e23c80f9def84c82e0a9761f3c8510a
David Kim <hyomithetech@gmail.com>
2020-10-22 03:48:11 -0700

    Feature: start-button - Add functionalities

--- src/app/IntroPage/IntroPage.js
+++ src/app/IntroPage/IntroPage.js
@@ -10,10 +10,16 @@ function Heading() {
         <span className="heading__primary-main">&nbsp;David Kim</span>
         <span className="heading__primary-sub">SOFTWARE ENGINEER</span>
       </div>
-      <div className="scroll-button">
-        <a href="#about">&nbsp;START</a>
-      </div>
-      <img src="./assets/images/logos/down-chevron.svg" alt="down arrow" />
+
+      <a href="#about" className="scroll-button">
+        &nbsp;START
+      </a>
+
+      <img
+        className="down-arrow"
+        src="./assets/images/logos/down-chevron.svg"
+        alt="down arrow"
+      />
     </header>
   );
 }
