f6522e416188220e3a1349a7eaa64041449710ef
David Kim <hyomithetech@gmail.com>
2020-10-22 03:40:47 -0700

    Feature: Navigation - Fix Navigation pane toggle functionality

--- src/app/Navigation/Navigation.js
+++ src/app/Navigation/Navigation.js
@@ -20,68 +20,62 @@ const Navigation = () => {
       //     document.body.style.overflow = "auto";
       //   }, 180);
     }
+
+    let checkbox = document.querySelector(".navigation__checkbox");
+
+    if (checkbox.checked) {
+      checkbox.checked = false;
+    } else {
+      checkbox.checked = true;
+    }
   };
 
   return (
     <div className="navigation">
-      <input
-        type="checkbox"
-        className="navigation__checkbox"
-        id="navi-toggle"
-      />
+      <input type="checkbox" className="navigation__checkbox" />
 
-      <label
-        htmlFor="navi-toggle"
-        className="navigation__button"
-        onClick={CheckboxHandler}
-      >
+      <label className="navigation__button" onClick={CheckboxHandler}>
         <span className="navigation__icon">&nbsp;</span>
       </label>
 
       <nav className="navigation__nav">
-        <label htmlFor="navi-toggle">
-          <ul className="navigation__list">
-            <li className="navigation__item">
-              <a
-                href="#home"
-                className="navigation__link"
-                onClick={CheckboxHandler}
-              >
-                HOME
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a
-                href="#about"
-                className="navigation__link"
-                htmlFor="navi-toggle"
-                onClick={CheckboxHandler}
-              >
-                ABOUT
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a
-                href="#portfolio"
-                className="navigation__link"
-                htmlFor="navi-toggle"
-                onClick={CheckboxHandler}
-              >
-                PORTFOLIO
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a
-                href="#contact"
-                className="navigation__link"
-                htmlFor="navi-toggle"
-                onClick={CheckboxHandler}
-              >
-                CONTACT ME
-              </a>
-            </li>
-          </ul>
-        </label>
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <a href="#" className="navigation__link" onClick={CheckboxHandler}>
+              HOME
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a
+              href="#about"
+              className="navigation__link"
+              htmlFor="navi-toggle"
+              onClick={CheckboxHandler}
+            >
+              ABOUT
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a
+              href="#portfolio"
+              className="navigation__link"
+              htmlFor="navi-toggle"
+              onClick={CheckboxHandler}
+            >
+              PORTFOLIO
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a
+              href="#contact"
+              className="navigation__link"
+              htmlFor="navi-toggle"
+              onClick={CheckboxHandler}
+            >
+              CONTACT ME
+            </a>
+          </li>
+        </ul>
       </nav>
     </div>
   );
