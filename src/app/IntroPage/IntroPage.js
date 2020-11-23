b22cc595d0b2676e37fbf7cef4c535fbe0f9a1ae
David Kim <hyomithetech@gmail.com>
2020-11-23 05:53:05 -0800

    Complete changing IntroPage display from grid to flexbox

--- src/app/IntroPage/IntroPage.js
+++ src/app/IntroPage/IntroPage.js
@@ -5,21 +5,21 @@ import LogoContainer from "./LogoContainer";
 function Heading() {
   return (
     <header className="landing-page" id="home">
-      {/* <LogoContainer /> */}
       <div className="heading__primary">
         <span className="heading__primary-main">&nbsp;David Kim</span>
         <span className="heading__primary-sub">SOFTWARE ENGINEER</span>
       </div>
+      <div className="start-button">
+        <a href="#about" className="start-button__label">
+          &nbsp;START
+        </a>
 
-      <a href="#about" className="scroll-button">
-        &nbsp;START
-      </a>
-
-      <img
-        className="down-arrow"
-        src="./assets/images/logos/down-chevron.svg"
-        alt="down arrow"
-      />
+        <img
+          className="start-button__icon"
+          src="./assets/images/logos/down-chevron.svg"
+          alt="down arrow"
+        />
+      </div>
     </header>
   );
 }
