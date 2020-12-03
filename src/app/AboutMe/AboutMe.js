b7d0a98a095b3beef078a4b16aa863f69f8d53d6
David Kim <hyomithetech@gmail.com>
2020-12-02 22:36:10 -0600

    Adjust size of project image and caption

--- src/app/AboutMe/AboutMe.js
+++ src/app/AboutMe/AboutMe.js
@@ -5,8 +5,13 @@ const AboutMe = () => {
     <section className="about" id="about">
       <h1 className="heading__secondary">ABOUT ME</h1>
 
-      {/* <h2 className="testing">Hi there, My name is David Kim.</h2> */}
-      <p className="about-me">
+      <img
+        className="about__profile"
+        src="./assets/images/picture/profile.jpeg"
+        alt="profile"
+      />
+
+      <p className="about__intro">
         Hi there, my name is David Kim, and I am a Full-Stack web and mobile
         developer with a passion for building creative software that improves
         the world through innovative technologies.
